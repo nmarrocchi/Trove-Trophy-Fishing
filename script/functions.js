e65a67915ca8ccadc99882029e9747af809a1344
nmarrocchi <nmarrocchi@la-providence.net>
2021-04-09 16:01:48 +0200

    Add img & Functions js file

---
+++ script/functions.js
@@ -0,0 +1,9 @@
+function show_fish_subnav()
+{
+    document.getElementById('fish_subnav').style.display = 'inline' ;
+}
+
+function hide_fish_subnav()
+{
+    document.getElementById('fish_subnav').style.display = 'none' ;
+}
